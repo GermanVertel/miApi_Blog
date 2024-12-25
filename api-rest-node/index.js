327170073d078f2570de19f26a5d9e6c133394e3
GermanVertel <84603164+GermanVertel@users.noreply.github.com>
2024-12-24 21:27:58 -0500

    se instalan nuevamente las dependencias

--- api-rest-node/index.js
+++ api-rest-node/index.js
@@ -1,9 +1,26 @@
 const { conexion } = require("./database/conexion");
-
+const express = require('express');
+const cors = require('cors');
 
 // inicializar app
 console.log('Hello, Mundo!');
 
 // Conexión a la base de datos  
-
 conexion();
+
+// crear el servidor de node
+const app = express();
+const puerto = 3900;
+
+// configurar cors
+app.use(cors());
+
+//convertir body a json
+app.use(express.json());
+
+// crear rutas
+
+// crear servidor y escuchar peticiones http
+app.listen(puerto, () => {
+    console.log('Servidor corriendo en el puerto ' + puerto);
+});
